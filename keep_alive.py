5eb8eea484eba5527fc1548ccda3103fa2be0e96
Protato_Plus <77544527+ProtatoPlus@users.noreply.github.com>
2022-05-05 14:58:07 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -17,13 +17,10 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
